dc1c22e5172519c5feb053d80664480762ee3d6e
Systronik <entwicklung@systronik.de>
2014-09-02 12:32:27 +0200

    js file updated

--- www/blecommunication.js
+++ www/blecommunication.js
@@ -6,12 +6,12 @@ var blecommunication = {
         cordova.exec(successCallback, errorCallback, "BLECommunication", "stopScanDevices");
     },
     connectDevice: function(address, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "BLECommunication", "scanDevices" , [{
+        cordova.exec(successCallback, errorCallback, "BLECommunication", "connectDevice" , [{
         	"address": address
         }]);
     },
     sendData: function(data, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "BLECommunication", "scanDevices", [{
+        cordova.exec(successCallback, errorCallback, "BLECommunication", "sendData", [{
         	"data": data
         }]);
     }
